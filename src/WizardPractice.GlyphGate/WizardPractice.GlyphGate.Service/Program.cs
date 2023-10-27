4380fff6b06c4d7e454dc71d2295b193d8fb7941
Scott Falbo <scottfalboart@gmail.com>
2023-10-27 16:26:10 -0700

    working on docker configs for self signing cert

--- src/WizardPractice.GlyphGate/WizardPractice.GlyphGate.Service/Program.cs
+++ src/WizardPractice.GlyphGate/WizardPractice.GlyphGate.Service/Program.cs
@@ -3,6 +3,7 @@
 // ------------------------------------
 
 using ProtoBuf.Grpc.Server;
+using System.Net;
 using WizardPractice.GlyphGate.Service;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddCodeFirstGrpc();
+
+var certificatePassword = builder.Configuration.GetValue<string>("Password");
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 80); // HTTP
+    options.Listen(IPAddress.Any, 443, listenOptions =>
+    {
+        listenOptions.UseHttps("aspnetapp.pfx", certificatePassword);
+    });
+});
 
 var app = builder.Build();
 
--- src/WizardPractice.ReagentReservoir/WizardPractice.ReagentReservoir.Service/Program.cs
+++ src/WizardPractice.ReagentReservoir/WizardPractice.ReagentReservoir.Service/Program.cs
@@ -3,6 +3,7 @@
 // ------------------------------------
 
 using ProtoBuf.Grpc.Server;
+using System.Net;
 using WizardPractice.ReagentReservoir.Service;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddCodeFirstGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 80); // HTTP
+    options.Listen(IPAddress.Any, 443, listenOptions =>
+    {
+        listenOptions.UseHttps("aspnetapp.pfx", "your-password"); // Replace "your-password" with your actual password
+    }); // HTTPS
+});
 
 var app = builder.Build();
 
